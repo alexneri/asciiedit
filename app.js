785279fdc5b6c066e4e89fb89468f4fef02b7cd7
alexneri <alex-ext@neri.kujata.jp>
2023-06-16 10:01:24 +0000

    Modified package dependencies for node and added some useful information at the home page

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// ASCIIDOC EDITOR
+// Written by Alexander Neri 04 June 2023
+// github.com/alexneri/asciiedit
+
 const express = require('express');
 const asciidoctor = require('asciidoctor')();
 const TurndownService = require('turndown');
@@ -9,29 +13,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 app.post('/preview', express.text({ type: '*/*' }), (req, res) => {
-    const asciidocSource = req.body;
-    const html = asciidoctor.convert(asciidocSource);
-    res.send(html);
+  const asciidocSource = req.body;
+  const html = asciidoctor.convert(asciidocSource);
+  res.send(html);
 });
 
 app.post('/reverse', express.text({ type: '*/*' }), (req, res) => {
-    const html = req.body;
-    const turndownService = new TurndownService();
-    const markdown = turndownService.turndown(html);
+  const html = req.body;
+  const turndownService = new TurndownService();
+  const markdown = turndownService.turndown(html);
 
-    exec(`echo "${markdown}" | pandoc -f markdown -t asciidoc`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        res.send(stdout);
-    });
+  exec(`echo "${markdown}" | pandoc -f markdown -t asciidoc`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    res.send(stdout);
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Asciidoc Editor listening at http://localhost:${port}`);
+  console.log(`Asciidoc Editor listening at http://localhost:${port}`);
 });
