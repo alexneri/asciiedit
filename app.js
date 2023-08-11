42b9bc373f66a67e95a11e02b2cdeb70249788cf
Alex Neri <alex-ext@neri.kujata.jp>
2023-08-12 00:00:54 +0200

    Replaced conversion engine with OPENAPI. API is working, but needs refinement.

--- app.js
+++ app.js
@@ -3,7 +3,34 @@
 // github.com/alexneri/asciiedit
 
 const express = require('express');
-const asciidoctor = require('asciidoctor')();
+
+
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  organization: 'YOUROrganizationID',
+  apiKey: 'YOURAPIKEY'
+});
+const ai = new OpenAIApi(configuration);
+// Import the OpenAI library
+
+
+
+function convertAsciidocToHtml(asciidoc, callback) {
+  // Send Asciidoc content to OpenAI API to convert to HTML
+  ai.complete({
+    prompt: 'Convert the following content to HTML:\n' + asciidoc,
+    max_tokens: 1000
+  }).then(response => callback(response.choices[0].text.trim()));
+}
+
+function convertHtmlToAsciidoc(html, callback) {
+  // Send HTML content to OpenAI API to convert to Asciidoc
+  ai.complete({
+    prompt: 'Convert this HTML content to Asciidoc:\n' + html,
+    max_tokens: 1000
+  }).then(response => callback(response.choices[0].text.trim()));
+}
+
 const TurndownService = require('turndown');
 const { exec } = require('child_process');
 const app = express();
@@ -16,10 +43,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/preview', express.text({ type: '*/*' }), (req, res) => {
+app.post('/preview', express.text({ type: '*/*' }), async (req, res) => {
   const asciidocSource = req.body;
-  const html = asciidoctor.convert(asciidocSource);
-  res.send(html);
+
+
+  try {
+    // Convert asciidocSource to HTML using OpenAI API
+    const gpt3_response = await ai.createCompletion({
+      model: 'text-davinci-002',
+      prompt: asciidocSource,
+      max_tokens: 500
+    });
+
+    let html = gpt3_response.data.choices[0].text;
+    res.send(html);
+  } catch (error) {
+    console.error("Error converting Asciidoc to HTML using OpenAI:", error);
+    res.status(500).send("Error converting content.");
+  }
+
 });
 
 app.post('/reverse', express.text({ type: '*/*' }), (req, res) => {
